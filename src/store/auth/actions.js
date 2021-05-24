feaeb48b7afc8aaf86aa3126631621a63f4c02a9
Daniil Mitrofanov <mdd-inbox@ya.ru>
2021-05-24 03:10:47 +0200

    Updated authRefreshSession method

--- src/store/auth/actions.js
+++ src/store/auth/actions.js
@@ -4,10 +4,6 @@ export const AUTH_REFRESH_SESSION = 'AUTH/REFRESH_SESSION';
 export const AUTH_REFRESH_SESSION_SUCCEED = 'AUTH/REFRESH_SESSION/SUCCEED';
 export const AUTH_REFRESH_SESSION_FAILED = 'AUTH/REFRESH_SESSION/FAILED';
 
-const extractRole = (token) => {
-	return 'role';
-}
-
 export const authRefreshSession = () => {
 	return async (dispatch) => {
 		dispatch({type: AUTH_REFRESH_SESSION});
@@ -16,14 +12,14 @@ export const authRefreshSession = () => {
 		if(response.success) {
 			dispatch({
 				type: AUTH_REFRESH_SESSION_SUCCEED,
-				token: response.token,
-				role: extractRole(response.token),
+				token: response.payload.token,
+				role: response.payload.role,
 			});
 		}else{
 			dispatch({
 				type: AUTH_REFRESH_SESSION_FAILED,
-				errorCode: response.errorCode,
-				errorMessage: response.errorMessage,
+				errorCode: response.payload.errorCode,
+				errorMessage: response.payload.errorMessage,
 			});
 		}
 	}
