b9359905d69c1b01afdde18d93815c0a525656a9
Daniil Mitrofanov <mdd-inbox@ya.ru>
2021-05-28 19:43:23 +0200

    Personal matrix SVG

--- src/components/calculator/Personal.jsx
+++ src/components/calculator/Personal.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { PersonalMatrix } from './controls/PersonalMatrix';
 
 export const Personal = () => {
 	return (
 		<div>
 			<h1>Personal</h1>
+
+			<div className="row">
+				<div className="col-5">
+					<PersonalMatrix/>
+				</div>
+			</div>
+
 		</div>
 	);
 };
---
+++ src/components/calculator/controls/PersonalMatrix.jsx
@@ -0,0 +1,286 @@
+import React from 'react';
+import pBackgroundSvg from '../../../assets/images/p-background.svg';
+
+export const PersonalMatrix = () => {
+	return (
+		<svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 304.27 264.58">
+			<image width="100%" height="100%" xlinkHref={pBackgroundSvg}/>
+			<text x="150.329" y="97.575" fill="#fff" fontSize="6.35" fontWeight="bold" textAnchor="middle">
+				26
+			</text>
+			<g style={{fontSize: '0.265rem'}} fontWeight="600">
+				<text x="26.152" y="115.656">
+					31
+				</text>
+				<text x="29.791" y="106.868">
+					32
+				</text>
+				<text x="33.43" y="98.079">
+					33
+				</text>
+				<text x="36.737" y="89.622">
+					34
+				</text>
+				<text x="40.708" y="80.501">
+					35
+				</text>
+				<text x="44.347" y="71.712">
+					36
+				</text>
+				<text x="47.987" y="62.923">
+					37
+				</text>
+				<g textAnchor="middle">
+					<text x="80.521" y="33.827">
+						38
+					</text>
+					<text x="89.31" y="30.188">
+						39
+					</text>
+					<text x="98.099" y="26.549">
+						40
+					</text>
+					<text x="106.888" y="22.578">
+						41
+					</text>
+					<text x="115.676" y="19.271">
+						42
+					</text>
+					<text x="124.465" y="15.631">
+						43
+					</text>
+					<text x="133.254" y="11.992">
+						44
+					</text>
+					<text x="168.378" y="11.992">
+						45
+					</text>
+					<text x="177.167" y="15.632">
+						46
+					</text>
+					<text x="185.956" y="19.271">
+						47
+					</text>
+					<text x="194.745" y="22.578">
+						48
+					</text>
+					<text x="203.534" y="26.549">
+						49
+					</text>
+					<text x="212.322" y="30.188">
+						50
+					</text>
+					<text x="221.111" y="33.827">
+						51
+					</text>
+				</g>
+				<text x="270.724" y="115.656">
+					58
+				</text>
+				<text x="267.085" y="106.868">
+					57
+				</text>
+				<text x="263.446" y="98.079">
+					56
+				</text>
+				<text x="260.139" y="89.622">
+					55
+				</text>
+				<text x="256.168" y="80.501">
+					54
+				</text>
+				<text x="252.529" y="71.712">
+					53
+				</text>
+				<text x="248.889" y="62.923">
+					52
+				</text>
+				<text x="270.724" y="152.202">
+					59
+				</text>
+				<text x="267.085" y="160.991">
+					60
+				</text>
+				<text x="263.446" y="169.78">
+					61
+				</text>
+				<text x="260.139" y="178.237">
+					62
+				</text>
+				<text x="256.168" y="187.358">
+					63
+				</text>
+				<text x="252.529" y="196.146">
+					64
+				</text>
+				<text x="248.889" y="204.935">
+					65
+				</text>
+				<g textAnchor="middle">
+					<text x="221.111" y="234.031">
+						66
+					</text>
+					<text x="212.322" y="237.67">
+						67
+					</text>
+					<text x="203.533" y="241.31">
+						68
+					</text>
+					<text x="194.745" y="245.281">
+						69
+					</text>
+					<text x="185.956" y="248.588">
+						70
+					</text>
+					<text x="177.167" y="252.227">
+						71
+					</text>
+					<text x="168.378" y="255.866">
+						72
+					</text>
+					<text x="133.254" y="255.866">
+						73
+					</text>
+					<text x="124.465" y="252.227">
+						74
+					</text>
+					<text x="115.676" y="248.588">
+						75
+					</text>
+					<text x="106.888" y="245.281">
+						76
+					</text>
+					<text x="98.099" y="241.31">
+						77
+					</text>
+					<text x="89.31" y="237.67">
+						78
+					</text>
+					<text x="80.521" y="234.031">
+						79
+					</text>
+				</g>
+				<text x="45.87" y="204.935">
+					80
+				</text>
+				<text x="44.002" y="196.196">
+					81
+				</text>
+				<text x="38.591" y="187.358">
+					82
+				</text>
+				<text x="34.62" y="178.237">
+					83
+				</text>
+				<text x="31.313" y="169.78">
+					84
+				</text>
+				<text x="27.674" y="160.991">
+					85
+				</text>
+				<text x="24.035" y="152.202">
+					86
+				</text>
+			</g>
+			<g fill="#fff" fontSize="9.878" fontWeight="bold" textAnchor="middle">
+				<text x="39.528" y="135.602">
+					1
+				</text>
+				<text x="150.688" y="24.677">
+					3
+				</text>
+				<text x="261.707" y="135.749">
+					5
+				</text>
+				<text x="150.688" y="246.939">
+					7
+				</text>
+			</g>
+			<g fontSize="9.878" fontWeight="bold" textAnchor="middle">
+				<text x="72.153" y="214.276">
+					8
+				</text>
+				<text x="72.185" y="57.239">
+					2
+				</text>
+				<text x="229.222" y="57.207">
+					4
+				</text>
+				<text x="229.19" y="214.245">
+					6
+				</text>
+				<text x="150.688" y="136.605">
+					30
+				</text>
+			</g>
+			<text x="61.333" y="134.988" fill="#fff" fontSize="7.761" fontWeight="bold" textAnchor="middle">
+				9
+			</text>
+			<text x="150.665" y="45.752" fill="#fff" fontSize="7.761" fontWeight="bold" textAnchor="middle">
+				11
+			</text>
+			<g fontWeight="bold" textAnchor="middle">
+				<g fontSize="7.761">
+					<text x="239.858" y="134.988">
+						13
+					</text>
+					<text x="150.665" y="224.358">
+						15
+					</text>
+					<text x="213.792" y="198.152">
+						14
+					</text>
+					<text x="87.565" y="198.089">
+						16
+					</text>
+					<text x="87.597" y="71.92">
+						10
+					</text>
+					<text x="214.07" y="71.584">
+						12
+					</text>
+				</g>
+				<text x="97.871" y="81.707" fontSize="6.35">
+					18
+				</text>
+				<text x="203.765" y="81.371" fontSize="6.35">
+					20
+				</text>
+			</g>
+			<text x="225.292" y="134.486" fill="#fff" fontSize="6.35" fontWeight="bold" textAnchor="middle">
+				21
+			</text>
+			<g fontSize="6.35" fontWeight="bold" textAnchor="middle">
+				<text x="203.488" y="187.361">
+					22
+				</text>
+				<text x="201.916" y="162.575">
+					27
+				</text>
+				<text x="182.225" y="166.413">
+					28
+				</text>
+				<text x="179.393" y="185.172">
+					29
+				</text>
+			</g>
+			<text x="150.651" y="209.304" fill="#fff" fontSize="6.35" fontWeight="bold" textAnchor="middle">
+				23
+			</text>
+			<text x="97.84" y="187.297" fontSize="6.35" fontWeight="bold" textAnchor="middle">
+				24
+			</text>
+			<g fill="#fff" fontSize="6.35" fontWeight="bold" textAnchor="middle">
+				<text x="75.87" y="134.486">
+					17
+				</text>
+				<text x="113.26" y="134.486">
+					25
+				</text>
+				<text x="150.651" y="59.802">
+					19
+				</text>
+			</g>
+		</svg>
+	);
+};
