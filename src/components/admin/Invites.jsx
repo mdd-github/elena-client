9cf6b491dc91af236161754da61e45bfbcbaee43
Daniil Mitrofanov <mdd-inbox@ya.ru>
2021-06-01 17:35:34 +0200

    Invites create/remove action

--- src/components/admin/Invites.jsx
+++ src/components/admin/Invites.jsx
@@ -3,7 +3,7 @@ import { NavBreadcrumbs } from './controls/NavBreadcrumbs';
 import s from '../../assets/scss/components/Navigator.module.scss';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { invitesGetAll } from '../../store/invites/actions';
+import { invitesCreate, invitesGetAll, invitesRemove } from '../../store/invites/actions';
 
 export const Invites = () => {
 	const invites = useSelector(state => state.invites);
@@ -17,8 +17,11 @@ export const Invites = () => {
 	return (
 		<div className="container">
 			<div className="row">
-				<div className="col-12 mt-3 mb-3">
+				<div className="col-10 mt-3 mb-3">
 					<NavBreadcrumbs/>
+				</div>
+				<div className="col-2 mt-3 mb-3 text-end">
+					<Link to="#" onClick={()=>dispatch(invitesCreate())} className="btn btn-success btn-sm">Сгенерировать</Link>
 				</div>
 			</div>
 
@@ -44,7 +47,7 @@ export const Invites = () => {
 									<td>{invite.value}</td>
 									<td>{date.getDate()}/{date.getMonth() + 1}/{date.getFullYear()}</td>
 									<td>
-										<Link to="#" >Удалить</Link>
+										<Link to="#" onClick={() => dispatch(invitesRemove(invite.value))}>Удалить</Link>
 									</td>
 								</tr>
 							)})
--- src/store/invites/actions.js
+++ src/store/invites/actions.js
@@ -1,14 +1,23 @@
 import { sendGET } from '../api/sendGet';
+import { sendPOST } from '../api/sendPost';
 
 export const INVITES_GET_ALL = 'INVITES/GET_ALL';
 export const INVITES_GET_ALL_SUCCEED = 'INVITES/GET_ALL/SUCCEED';
 export const INVITES_GET_ALL_FAILED = 'INVITES/GET_ALL/FAILED';
 
+export const INVITES_REMOVE = 'INVITES/REMOVE';
+export const INVITES_REMOVE_SUCCEED = 'INVITES/REMOVE/SUCCEED';
+export const INVITES_REMOVE_FAILED = 'INVITES/REMOVE/FAILED';
+
+export const INVITES_CREATE = 'INVITES/CREATE';
+export const INVITES_CREATE_SUCCEED = 'INVITES/CREATE/SUCCEED';
+export const INVITES_CREATE_FAILED = 'INVITES/CREATE/FAILED';
+
 export const invitesGetAll = () => {
 	return async(dispatch, getState) => {
 		dispatch({type: INVITES_GET_ALL});
 
-		const response = await sendGET('invite/all/', {
+		const response = await sendGET('invite/all', {
 			'Authorization': 'Bearer ' + getState().auth.token
 		});
 
@@ -24,5 +33,53 @@ export const invitesGetAll = () => {
 				errorMessage: response.payload.message,
 			});
 		}
+	}
+}
+
+export const invitesRemove = (invite) => {
+	return async(dispatch, getState) => {
+		dispatch({type: INVITES_REMOVE});
+
+		const response = await sendPOST('invite/remove/' + invite, {}, {
+			'Authorization': 'Bearer ' + getState().auth.token
+		});
+
+		if(response.success) {
+			dispatch({
+				type: INVITES_REMOVE_SUCCEED,
+			});
+		} else {
+			dispatch({
+				type: INVITES_REMOVE_FAILED,
+				errorCode: response.payload.code,
+				errorMessage: response.payload.message,
+			});
+		}
+
+		dispatch(invitesGetAll());
+	}
+}
+
+export const invitesCreate = () => {
+	return async(dispatch, getState) => {
+		dispatch({type: INVITES_CREATE});
+
+		const response = await sendPOST('invite/create', {}, {
+			'Authorization': 'Bearer ' + getState().auth.token
+		});
+
+		if(response.success) {
+			dispatch({
+				type: INVITES_CREATE_SUCCEED,
+			});
+		} else {
+			dispatch({
+				type: INVITES_CREATE_FAILED,
+				errorCode: response.payload.code,
+				errorMessage: response.payload.message,
+			});
+		}
+
+		dispatch(invitesGetAll());
 	}
 }
