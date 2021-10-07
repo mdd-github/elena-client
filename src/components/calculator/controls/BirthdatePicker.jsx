40ef79463c4e4c9ee16ad71bc4fbf7ecfc331141
Daniil Mitrofanov <mdd-inbox@ya.ru>
2021-10-07 18:07:30 +0200

    Submit changes button

--- src/components/calculator/controls/BirthdatePicker.jsx
+++ src/components/calculator/controls/BirthdatePicker.jsx
@@ -36,17 +36,17 @@ export const BirthdatePicker = ({onChangeValue, initialDate, name, setName}) => 
 
 	return (
 		<div className="row mt-md-3 mt-lg-0">
-			<div className="col-12 col-md-5 p-0 my-3 my-md-0 ">
+			<div className="col-12 col-md-3 p-0 my-3 my-md-0 ">
 				<input type="text" className="form-control form-select-sm" value={name}
 							 onChange={changeName} placeholder="Имя"/>
 			</div>
 			<div className="col-3 col-md-2 pe-0 ps-0 ps-md-3">
 				<input type="text" className="form-control form-select-sm" value={date}
-							 onChange={changeDate} onBlur={changeValue}/>
+							 onChange={changeDate}/>
 			</div>
 			<div className="col-6 col-md-3">
 				<select className="form-select form-select-sm" value={month}
-								onChange={changeMonth} onBlur={changeValue}>
+								onChange={changeMonth}>
 					<option value="0">Январь</option>
 					<option value="1">Февраль</option>
 					<option value="2">Март</option>
@@ -63,7 +63,14 @@ export const BirthdatePicker = ({onChangeValue, initialDate, name, setName}) => 
 			</div>
 			<div className="col-3 col-md-2 col-2 p-0">
 				<input type="text" className="form-control form-select-sm" value={year}
-							 onChange={changeYear} onBlur={changeValue}/>
+							 onChange={changeYear}/>
+			</div>
+
+			<div className="col-12 col-md-2 pe-0 ps-0 ps-md-3 mt-3 mt-md-0">
+				<button type="button" className="btn btn-sm btn-primary w-100"
+				onClick={changeValue}>
+					Применить
+				</button>
 			</div>
 		</div>
 	);
